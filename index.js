79c59cc146c6f802fe38340a2bfe8703bb5a5d24
ismaelvr1999 <ismael.villalpando@alumnos.udg.mx>
2024-11-03 18:00:35 -0600

    init commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const dropArea = document.getElementById("drop-area");
+const track = document.getElementById("track");
+const playButton = document.getElementById("play-button");
+const stopButton = document.getElementById("stop-button")
+
+dropArea.addEventListener("dragover",(ev)=>{
+    ev.preventDefault();
+});
+
+const handlerDrop = (ev) =>{
+    ev.preventDefault();
+    const trackFile = ev.dataTransfer.files[0];
+    const urlFile = URL.createObjectURL(trackFile);
+    track.setAttribute("src",urlFile);
+}
+
+dropArea.addEventListener("drop",handlerDrop);
+
+//Todo: Progress bar
+track.addEventListener("timeupdate",(e)=>{
+    //console.log(song.currentTime);
+});
+
+playButton.addEventListener("click",()=>{
+    if(track.readyState == 4){
+        track.play();
+    }
+});
+
+stopButton.addEventListener("click",()=>{
+    if(track.readyState == 4){
+        track.pause();
+    }
+});
+
+
+
+
+
