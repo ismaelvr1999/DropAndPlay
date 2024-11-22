a773f6492b52cf3fb7bcd6913103af63c42f0345
ismaelvr1999 <ismael.villalpando@alumnos.udg.mx>
2024-11-21 20:12:06 -0600

    Feature: Progress bar and Title song added

--- index.js
+++ index.js
@@ -1,39 +1,58 @@
+//https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement
 const dropArea = document.getElementById("drop-area");
 const track = document.getElementById("track");
-const playButton = document.getElementById("play-button");
-const pauseButton = document.getElementById("pause-button")
+const playIcon = document.getElementById("play-icon");
+const pauseIcon = document.getElementById("pause-icon");
+const playAndPauseBtn = document.getElementById("play-and-pause-button");
+const titleSong = document.getElementById("song-title");
+const progressBar = document.getElementById("progress-bar");
 
-dropArea.addEventListener("dragover",(ev)=>{
-    ev.preventDefault();
-});
-
-const handlerDrop = (ev) =>{
-    ev.preventDefault();
-    const trackFile = ev.dataTransfer.files[0];
-    const urlFile = URL.createObjectURL(trackFile);
-    track.setAttribute("src",urlFile);
+const resetButtonsStyles = ()=>{
+    playIcon.classList.remove("hide-icon");
+    pauseIcon.classList.add("hide-icon");
 }
 
-dropArea.addEventListener("drop",handlerDrop);
+const handlerDrop = (ev) => {
+  ev.preventDefault();
+  const trackFile = ev.dataTransfer.files[0];
+  const urlFile = URL.createObjectURL(trackFile);
 
-//Todo: Progress bar
-track.addEventListener("timeupdate",(e)=>{
-    //console.log(song.currentTime);
+  titleSong.innerHTML = trackFile.name;
+  track.setAttribute("src", urlFile);
+  resetButtonsStyles();
+
+};
+dropArea.addEventListener("dragover", (ev) => {
+  ev.preventDefault();
 });
 
-playButton.addEventListener("click",()=>{
-    if(track.readyState == 4){
-        track.play();
-    }
+dropArea.addEventListener("drop", handlerDrop);
+
+track.addEventListener("canplaythrough",(e)=>{
+  progressBar.max = track.duration;
+})
+
+track.addEventListener("timeupdate", (e) => {
+  progressBar.value = track.currentTime;
 });
 
-pauseButton.addEventListener("click",()=>{
-    if(track.readyState == 4){
-        track.pause();
-    }
+playAndPauseBtn.addEventListener("click", () => {
+  // Play
+  if (track.readyState === 4 && track.paused === true) {
+    track.play();
+    //hide the play icon
+    playIcon.classList.toggle("hide-icon");
+    //show the pause icon
+    pauseIcon.classList.toggle("hide-icon");
+  }
+  //Pause
+  else if (track.readyState === 4 && track.paused === false) {
+    track.pause();
+    //show the play icon
+    playIcon.classList.toggle("hide-icon");
+    //hide the pause icon
+    pauseIcon.classList.toggle("hide-icon");
+  } 
 });
 
-
-
-
-
+//TODO: Volumen Control
